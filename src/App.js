0fe425a82ea624fde4a5a9a1e65d7ebe66aebde4
Jason Law <jaylaw81@gmail.com>
2017-06-15 13:50:42 -0400

    updated layout

--- src/App.js
+++ src/App.js
@@ -1,72 +1,25 @@
 import React from 'react';
-import Header from './components/Header';
-import Loading from './components/Loading';
-import {connect} from 'react-redux';
-import {queryParams} from './common/services/url';
-import {setUserFromToken} from './common/actions/user';
-import { ENVIRONMENT, LOCAL_STORAGE } from './common/constants';
-import ErrorComponent from './components/ErrorComponent';
 
 class App extends React.Component {
-  loading = true;
-
   componentDidMount() {
-    const { routing, dispatch } = this.props;
-    // If it's in queryParams, it's a callback from our Auth API
-    let params = queryParams(routing.location.search);
-    let token = params.token || localStorage[LOCAL_STORAGE.TOKEN];
-    if (token) {
-      dispatch(setUserFromToken(token));
-    } else {
-      window.location.href = `${ENVIRONMENT.AUTH_API_ROOT}/auth/login`;
-    }
-  }
-
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   // TODO: See why dispatch.pushes aren't updating when location changes
-  //   return true;
-  // }
-
-  componentWillReceiveProps(nextProps) {
-    const { user } = this.props;
-    if(nextProps.user.error){
-      this.loading = false;
-    }else if (!user.userRole && nextProps.user.userRole) {
-      this.loading = false;
-    } else if((user.userRole && !nextProps.user.userRole)) {
-      // Logged out
-      window.location.href = `${ENVIRONMENT.AUTH_API_ROOT}/auth/login`;
-    }
+    const { routing } = this.props;
   }
 
   render() {
-    const {children, user} = this.props;
-    let errorLink = null;
-    if (user.error) {
-      errorLink = <a onClick={() => {
-        delete localStorage[LOCAL_STORAGE.TOKEN];
-        window.location.href = `${ENVIRONMENT.AUTH_API_ROOT}/auth/login`;
-      }} href="#">Click here to login again</a>;
-    }
+    const {children} = this.props;
     return (
       <div className="App">
-        <Header />
-        {!this.loading && !user.error && children}
-        {this.loading && <Loading />}
-        {user.error &&  <ErrorComponent
-          errorMessage={`You do not have access to Store Walk. Please contact your administrator and try again.`}
-          errorLink={errorLink} />}
+        {children}
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  const {user, routing} = state;
+  const {routing} = state;
   return {
-    user,
     routing
   };
 }
 
-export default connect(mapStateToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Home from './routes/Home';
-import Store from './routes/Store';
-import Survey from './routes/Survey';
-import Modal from './components/Modals';
-import SurveyDone from './components/SurveyDone';
-//import Route from './components/AuthRoute'; // TODO: Enable when we have Auth working
-import './common/assets/styles/index.css';
+import Test from './components/Test';
 
 // Redux and react-router
-import {Route} from 'react-router'; // TODO: Disable when we have Auth working
-import {ConnectedRouter} from 'react-router-redux';
-import {Provider} from 'react-redux'
-import configureStore, {history} from './common/store/configureStore';
-const store = configureStore();
+import {Route, Router} from 'react-router'; // TODO: Disable when we have Auth working
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
+    <BrowserRouter history={history}>
       <App>
-        <Route exact={true} path="/" component={Home}/>
-        <Route path="/store" component={Store}/>
-        <Route path="/survey" component={Survey} />
-        <Route path="/modal" component={Modal} />
-        <Route path="/done" component={SurveyDone} />
+        <Route exact={true} path="/" component={Test}/>
       </App>
-    </ConnectedRouter>
-  </Provider>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
