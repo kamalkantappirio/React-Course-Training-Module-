33b8d9e2adbdff263345faed9317c644ecce1ed4
Jason Law <jaylaw81@gmail.com>
2017-06-16 13:45:31 -0400

    Feature/test feature (#15)
    CRA app in Express

--- server/app.js
+++ server/app.js
@@ -2,24 +2,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const router = express.Router();
-
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "*");
+//   next();
+// });
 
 // Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+//app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-app.get('/api', (req, res) => {
-  res.json([{"hello": "World"}]);
+// Redirect all routes back to index.html, as this is simply serves up a SPA
+app.get('/[^\.]+$', function(req, res){
+  res.set('Content-Type', 'text/html')
+    .sendFile(__dirname + '/build/index.html');
 });
 
 
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,37 +10,16 @@ class App extends Component {
   static propTypes = {}
   static defaultProps = {}
   state = {
-    "food" : []
-  }
-
-  search(query) {
-    return fetch(`/api/?q=${query}`, {
-      accept: 'application/json',
-    }).then((response) => {
-      return response.json();
-    }).then((j) => {
-      // Yay, `j` is a JavaScript object
-      console.log(j); 
-      this.setState({
-        food: j[0].hello
-      })
-    });
-  }
-
-  componentDidMount() {
-    this.search();
-
   }
 
   render() {
     
     const { className } = this.props;
-    const { food } = this.state;
     return (
       <div className={classnames('App', className)}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React {food}</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
