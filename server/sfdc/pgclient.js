903b75e5b0f8168afa583dd6549386c256a5fce7
Jason Law <jaylaw81@gmail.com>
2017-07-13 09:12:12 -0400

    updated node version to LTS

--- server/sfdc/pgclient.js
+++ server/sfdc/pgclient.js
@@ -4,7 +4,7 @@ const knex = require('knex')({
         host : 'localhost',
         port:'5433',
         user : 'postgres',
-        password : 'appirio123',
+        password : '',
         database : 'postgres'
     },
     pool: { min: 0, max: 7 }
